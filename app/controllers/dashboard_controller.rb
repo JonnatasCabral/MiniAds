4d1ade334d4d1f9767124c95a2ed0929104331b2
Jonnatas Cabral <matiasjonnatas@gmail.com>
2016-03-31 22:59:12 -0300

    ending details view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,24 @@
 class DashboardController < ApplicationController
 
 	def index
-		@creatives = Ad.all
+		@ads = Ad.all
 	end
 
 	def details
-
+		@creatives = Creative.all
+		@targets = Targeting.all
 	end
+
+	def new
+		@ad = Ad.new
+		@creatives = Creative.new
+		@targeting = Targeting.new
+	end
+
+
+
+	private
+		def params
+	      params.require(:ad).permit(:budget)
+	    end
 end
