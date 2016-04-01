96b84d0161074495c9531f539a22365152fe7140
Jonnatas Cabral <matiasjonnatas@gmail.com>
2016-04-01 20:50:12 -0300

    starting the construction of the form

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,21 +4,47 @@ class DashboardController < ApplicationController
 		@ads = Ad.all
 	end
 
-	def details
+	def show
 		@creatives = Creative.all
 		@targets = Targeting.all
 	end
 
 	def new
 		@ad = Ad.new
-		@creatives = Creative.new
-		@targeting = Targeting.new
+		@ad.creative.build
+		@ad.targeting.build
+	end
+
+	def destroy
+		@ad.destroy
+    	respond_to do |format|
+      	format.html { redirect_to people_url, notice: 'Person was successfully destroyed.' }
+      	format.json { head :no_content }
+    end
+	end
+
+	def create
+    @ad = Ad.new(ad_params)
+
+	    respond_to do |format|
+	      if @ad.save
+	        format.html { redirect_to @ad, notice: 'ad was successfully created.' }
+	        format.json { render :show, status: :created, location: @ad }
+	      else
+	        format.html { render :new }
+	        format.json { render json: @ad.errors, status: :unprocessable_entity }
+	      end
+	    end
 	end
 
 
 
 	private
-		def params
-	      params.require(:ad).permit(:budget)
+		def set_ad
+      		@ad = Ad.find(params[:id])
+    	end
+
+		def ad_params
+	      params.require(:ad).permit(:budget, creative:[:bid, :adtext])
 	    end
 end
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -5,6 +5,9 @@ class Ad
   has_many :targeting, class_name:"Targeting"
   field :budget, type: Float
 
+  accepts_nested_attributes_for :creative,
+	reject_if: proc { |attributes| attributes['bid', 'adtext'].blank? }
+  accepts_nested_attributes_for :targeting
   validates :budget, numericality: { greater_than: 0 }
   validates :creative, :length => { :minimum => 1 }
   validates :targeting, :length => { :minimum => 1 }
