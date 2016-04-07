02895bece2a9f413104599449f6a1cef82e74d7b
Jonnatas Cabral <matiasjonnatas@gmail.com>
2016-04-07 18:35:09 -0300

    init tests

--- test/models/ad_test.rb
+++ test/models/ad_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AdTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	test "should not save new ad" do
+  	ad = Ad.new
+  	assert !ad.save, "Saved not the ad without creative and targeting"
+	end
+
 end
